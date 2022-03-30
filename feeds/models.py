06570bb4d3f110bfda812d029d128bd31ebf085f
Fredrick Ojure <fredojure@gmail.com>
2022-03-30 12:17:30 +0300

    8. Added a field for post heading

--- feeds/models.py
+++ feeds/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Post(models.Model):
+    heading = models.CharField(max_length=50)
     text = models.CharField(max_length=300)
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.text[0:30]
+        return self.heading[0:30]
