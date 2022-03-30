e4057b3aabae10b93644adc443da0535d7777277
Fredrick Ojure <fredojure@gmail.com>
2022-03-30 16:44:06 +0300

    11. Added Author data and thumbnail to templates

--- feeds/models.py
+++ feeds/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     heading = models.CharField(max_length=50)
     text = models.CharField(max_length=300)
     date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.heading[0:30]
