e8d994fb35af85468fbd08f213a40d016a9a0c94
Fredrick Ojure <fredojure@gmail.com>
2022-03-31 15:50:45 +0300

    14. Created the New Post process. Created the path, the form and updated links

--- feeds/views.py
+++ feeds/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
@@ -15,3 +17,19 @@ class PostDetailView(DetailView):
     template_name = 'feeds/detail.html'
     model = Post
     context_object_name = 'post'
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'feeds/create.html'
+    fields = ['heading', 'text']
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        obj = form.save(commit = False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
