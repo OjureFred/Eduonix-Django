dd67b425b36a0acc5aff9dfeb4dd2c5eaabb52dc
Fredrick Ojure <fredojure@gmail.com>
2022-04-01 18:41:23 +0300

    15. Enabled settings for static files and created main.js file

--- social/settings.py
+++ social/settings.py
@@ -161,3 +161,10 @@ AUTHENTICATION_BACKENDS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "frontend")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "static/"
