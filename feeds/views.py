b98cdc12a9252de72d273548f69c1e2950d5828f
Fredrick Ojure <fredojure@gmail.com>
2022-04-06 12:26:56 +0300

    23. Modified template pages for Follow and Unfollow process

--- feeds/views.py
+++ feeds/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
+from followers.models import Follower
 
-class HomePage(ListView):
+class HomePage(TemplateView):
     http_method_names = ['get']
     template_name = 'feeds/homepage.html'
-    model = Post
-    context_object_name = "posts"
-    queryset = Post.objects.all().order_by('-id')[0:30]
 
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+
+    def get_context_data(self, *args,  **kwargs):
+        context =  super().get_context_data(*args, **kwargs)
+
+        if self.request.user.is_authenticated:
+            following = list(Follower.objects.filter(followed_by = self.request.user).values_list('following', flat=True))
+            posts = Post.objects.filter(user_in = following).order_by('-id')[0:30]
+        else:
+            posts = Post.objects.all().order_by('-id')[0:30]
+        
+        context['posts'] = posts
+        return context
+    
 class PostDetailView(DetailView):
     http_method_names = ['get']
     template_name = 'feeds/detail.html'
