e941eff2be4b53548e557f08f5ac9bc49a108784
Fredrick Ojure <fredojure@gmail.com>
2022-04-14 11:26:23 +0300

    34. Created viewset for Comment model serializer

--- socialapi/serializers.py
+++ socialapi/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 
 from feeds.models import Post
+from comments.models import Comment
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
         fields = ('heading', 'text', 'date', 'author')
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('text', 'date', 'author', 'post')
--- socialapi/views.py
+++ socialapi/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import permissions
 
-from .serializers import PostSerializer
+from .serializers import PostSerializer, CommentSerializer
+
 from feeds.models import Post
-
+from comments.models import Comment
 
 class PostViewSet(viewsets.ModelViewSet):
+    '''
+    API endpoint to allow Posts to be viewed and edited
+    '''
     queryset = Post.objects.all().order_by('-id')
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    '''
+    API endpoint to allow Comments to be viewed and edited
+    '''
+
+    queryset = Comment.objects.all().order_by('post')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
